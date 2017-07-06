5b98ed257099a3cf70fb4e701531c6098fe876dd
Daniel Nery <danielnso@outlook.com>
2017-07-06 01:51:52 -0300

    :sparkles: Begin bluetooth
    - Only for constants

--- src/pages/constants/constants.ts
+++ src/pages/constants/constants.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { ViewController, NavParams } from 'ionic-angular';
+import { ViewController, NavParams, LoadingController, ToastController } from 'ionic-angular';
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
@@ -8,8 +9,22 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class ConstantsPage {
   constants: FormGroup;
+  mac: string;
+  bt_connection: any;
 
-  constructor(public viewCtrl: ViewController, public navParams: NavParams, public formBuilder: FormBuilder) {
+  constructor(public viewCtrl: ViewController, public navParams: NavParams, public formBuilder: FormBuilder, private bt: BluetoothSerial, private loadingCtrl: LoadingController, private toastCtrl: ToastController) {
+    this.bt.enable().then(() => {
+      console.log("BT enabled!");
+    }, (err) => {
+      console.log("BT not enabled: ", err);
+    });
+
+    this.bt.subscribe('\n').subscribe((data) => {
+      console.log("Received: " + data);
+    });
+
+    this.mac = '30:14:06:16:05:47';
+
     this.constants = formBuilder.group({
       kp: [''],
       ki: [''],
@@ -18,18 +33,54 @@ export class ConstantsPage {
   }
 
   save() {
-    console.log(this.constants.value['kp'])
-    console.log(this.constants.value['ki'])
-    console.log(this.constants.value['kd'])
+    let kp: number = this.constants.value['kp'];
+    let ki: number = this.constants.value['ki'];
+    let kd: number = this.constants.value['kd'];
+    console.log(kp);
+    console.log(ki);
+    console.log(kd);
+    this.send([255, 6, kp, kd, ki, 254]);
   }
 
   start() {
-      console.log('Robot started.')
-      // send start signal
+    console.log('Robot started.');
+    this.send([255, 4, 1, 254]);
   }
 
   stop() {
-      console.log('Robot stopped.')
-      // send stop signal
-}
+    console.log('Robot stopped.');
+    this.send([255, 4, 0, 254]);
+  }
+
+  connect() {
+     let loading = this.loadingCtrl.create({
+      content: 'Conectando...'
+    });
+
+    loading.present();
+
+    this.bt_connection = this.bt.connect(this.mac);
+    this.bt_connection.subscribe(() => {
+      loading.dismiss();
+      this.toastCtrl.create({
+        message: 'Conectado com Sucesso!',
+        duration: 3000
+      }).present();
+    }, (err) => {
+      loading.dismiss();
+      this.toastCtrl.create({
+        message: 'A conexao falhou!',
+        duration: 3000
+      }).present();
+      console.log("Connection failed: ", err);
+    });
+  }
+
+  send(arr: number[]) {
+    this.bt.write(arr).then(() => {
+      console.log("Enviado: ", arr);
+    }, (err) => {
+      console.log("Erro ao enviar: ", arr, err);
+    })
+  }
 }
--- src/pages/sensors/sensors.ts
+++ src/pages/sensors/sensors.ts
@@ -7,35 +7,23 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'sensors.html'
 })
 export class SensorsPage {
-  bt: any;
   mac: number;
   sensores: Array<{name: number, valor: number}>;
 
-  constructor(public navCtrl: NavController, private bts: BluetoothSerial) {
-    this.bt = bts.enable();
-    this.mac = 12
-    this.sensores = [
-        { name: 0,  valor: 0 },
-        { name: 1,  valor: 0 },
-        { name: 2,  valor: 0 },
-        { name: 3,  valor: 0 },
-        { name: 4,  valor: 0 },
-        { name: 5,  valor: 0 },
-        { name: 6,  valor: 0 },
-        { name: 7,  valor: 0 },
-        { name: 8,  valor: 0 },
-        { name: 9,  valor: 0 },
-        { name: 10, valor: 0 },
-        { name: 11, valor: 0 },
-        { name: 12, valor: 0 },
-        { name: 13, valor: 0 },
-        { name: 14, valor: 0 }
-    ];
+  constructor(public navCtrl: NavController, private bt: BluetoothSerial) {
+    this.sensores = [];
+    for (let i = 0; i < 15; i++) {
+      this.sensores.push(
+        { name: i,  valor: 0 }
+      );
+    }
   }
-  connect(){
-    this.bt.connect(this.mac);
-    this.bt.subscribe();
-  }
+
+  // connect(){
+  //   this.bt.connect(this.mac);
+  //   this.bt.subscribe();
+  // }
+
   getSensors() {
     for (var sensor of this.sensores){
       sensor.valor=(sensor.valor + 100)%1023;
