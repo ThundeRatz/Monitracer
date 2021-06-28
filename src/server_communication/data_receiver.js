3eacc89b39465ebaacc6fe0f8ba61a55f8909fb9
VanderSant <vanderson.santos@usp.br>
2021-06-28 06:17:24 -0300

    🚧  add data receiver prototype

--- src/server_communication/data_receiver.js
+++ src/server_communication/data_receiver.js
@@ -1,1 +1,22 @@
 import React from 'react';
+
+const sendHttpRequest = (method, url, data) =>{
+    const promise = new Promise(( resolve, reject) =>{
+        const xhr= new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.respondeType='json';
+        if(data){
+            xhr.setRequestHeader('Content-Type','application/json');
+        }
+        xhr.onload = () => {
+            resolve(xhr.response);
+        };
+        xhr.onerror = () => {
+            reject('Something went wrong!')
+        }
+        xhr.send(JSON.stringify(data));
+    });
+    return promise;
+};
+
+export default sendHttpRequest;
