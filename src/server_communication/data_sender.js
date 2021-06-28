4ee02b369420cb38141d095770336171c28742a2
VanderSant <vanderson.santos@usp.br>
2021-06-28 06:10:07 -0300

    🚧  add data sender prototype

--- src/server_communication/data_sender.js
+++ src/server_communication/data_sender.js
@@ -1,1 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { View, StatusBar, StyleSheet } from 'react-native'
+
+export default class data_sender extends Component{
+
+    async ComponentDidMount(props){
+        try{
+            await fetch(props.api,{
+                method: 'post',
+                mode: 'no-cors',
+                headers: {
+                    'Accept': '',
+                    'Content-Type': ''
+                },
+                body: JSON.stringify({
+                    data_test: 'neree'
+                })
+            });
+
+        } catch(e){
+            console.log(e)
+        }
+    };
+
+    render() {
+        return(
+            <View></View>
+        );
+    }
+}
