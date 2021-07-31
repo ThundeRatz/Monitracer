e5318ae4ecc28c8d25c7c9d6952ce2b1945ba13e
guedeslucas1 <guedeslucas@usp.br>
2021-07-31 11:05:32 -0300

    ⚡️ Implement data receiver with axios

--- src/server_communication/data_receiver.js
+++ src/server_communication/data_receiver.js
@@ -8,37 +8,19 @@
  * @date 07/2021
  */
 
-import React from 'react';
+import axios from 'axios'
 
-import {api,api_get,api_webhook} from './api_config'
-
-export const sendHttpRequest = (method, url, data) =>{
-    const promise = new Promise(( resolve, reject) =>{
-        const xhr= new XMLHttpRequest();
-        xhr.open(method, url);
-        xhr.respondeType='json';
-        if(data){
-            xhr.setRequestHeader('Content-Type','application/json');
-        }
-        xhr.onload = () => {
-            resolve(xhr.response);
-        };
-        xhr.onerror = () => {
-            reject('Something went wrong!')
-        }
-        xhr.send(JSON.stringify(data));
-    });
-    return promise;
-};
+import {api,api_get,api_webhook} from './api_config.js'
 
 export const GetData = async (props) =>{
-    var link = api_get;
-    console.log(link);
-    sendHttpRequest('GET',link)
-    .then(responseData=>{
-        var data = JSON.parse(responseData);
-        console.log(data);
-    });
+    let link = api_get;
+    
+    try {
+        let res = await axios.get(link);
+        console.log(res.data);
+        return(res.data)
+    } catch(error) {
+        console.log(error);
+        console.log("FAILED TO GET DATA")
+    }
 };
-
-
