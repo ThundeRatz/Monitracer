887cfa722bb4391af560d8b41dcb23db73dcc6a7
VanderSant <vanderson.santos@usp.br>
2021-08-02 03:59:28 -0300

    🐛 fix a data receiver api error

--- src/server_communication/api_config.js
+++ src/server_communication/api_config.js
@@ -1,4 +1,4 @@
 export const api = "https://api.thunderatz.org/tracer/constants";
-export const api_webhook ="https://webhook.site/0da26257-fc6f-4a4a-9b93-a500e9f6d8a3";
+export const api_webhook ="https://webhook.site/f8bf9acb-da3c-4b1b-bab8-7b07325e119c";
 export const api_get = "https://api.thunderatz.org/tracer/constant_types";
 
--- src/server_communication/data_sender.js
+++ src/server_communication/data_sender.js
@@ -15,26 +15,25 @@ import {api,api_webhook} from './api_config'
 import axios from 'axios';
 
 export const PostData = async (kp,kd,ki,lap_time) => {
-    let info = {
-        method: "POST",
+    let headers = {
         headers: {
             "Accept": "application/json",
             "Content-Type": "application/json"
-        },
-        body:{
-            "name": "neru",
-            "values":{
-                "1":parseFloat(kp),
-                "2":parseFloat(kd),
-                "3":parseFloat(ki)
-            },
         }
+    };
+
+    let data = {
+        "name": "teste",
+        "values":{
+            "1":parseFloat(kp),
+            "2":parseFloat(kd),
+            "3":parseFloat(ki)
+        },
     };
 
     try{
         console.log("sinal server: " + kp + kd + ki + lap_time)
-        console.log(info);
-        const res = await axios.post(api,{...info});
+        const res = await axios.post(api, data, headers);
         console.log(res.data);
     } catch(error){
         console.log(error);
