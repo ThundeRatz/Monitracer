ebdd147dbe50a0f650bdc1d2295181b475c1bfa8
DamaralHenrique <rique01101999@gmail.com>
2021-10-01 16:06:45 -0300

    🎨 Add colors palette file

---
+++ src/components/colors.js
@@ -0,0 +1,31 @@
+export const COLORS = {
+    /* Primary */
+    thunderBlue: "#011749",
+    lightThunderBlue: "#011749",
+
+    /* Secondary */
+    thunderYellow: "#FCE702",
+    lightThunderYellow: "#FCE702",
+
+    /* Appendant */
+    controllButton01: "#008000",
+    controllButton02: "#FF0000",
+    thunderBlack: "#000000",
+
+    /* Allert */
+    thunderAllert: "#E10000",
+
+    /* Error */
+    thunderError: "#AB1212",
+
+    /* Success */
+    thunderSuccess: "#047FF0",
+
+    /* Gray shade */
+    gray1: "#333333",
+    gray2: "#4F4F4F",
+    gray3: "#828282",
+    gray4: "#BDBDBD",
+    gray5: "#E0E0E0",
+    gray6: "#F2F2F2",
+}
