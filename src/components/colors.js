656ec79430f0175832215b2345f353d88a20ac4a
DamaralHenrique <rique01101999@gmail.com>
2021-11-01 17:36:15 -0300

    🙈 Correcting wrong colors codes

--- src/components/colors.js
+++ src/components/colors.js
@@ -1,11 +1,11 @@
 export const COLORS = {
-  /* Primary */
-  thunderBlue: '#011749',
-  lightThunderBlue: '#011749',
+    /* Primary */
+    thunderBlue: "#011749",
+    lightThunderBlue: "#535F7A",
 
-  /* Secondary */
-  thunderYellow: '#FCE702',
-  lightThunderYellow: '#FCE702',
+    /* Secondary */
+    thunderYellow: "#FCE702",
+    lightThunderYellow: "#F8ED7C",
 
   /* Appendant */
   controllButton01: '#008000',
