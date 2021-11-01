2f4d80e6d1804338b1428844882cb88f91218ead
DamaralHenrique <rique01101999@gmail.com>
2021-11-01 18:57:17 -0300

    🚧 Update home page to test new buttons

--- src/tabs/HomePage.js
+++ src/tabs/HomePage.js
@@ -20,10 +20,13 @@ import {
   CellTitleContainer,
   TextInputCell,
 } from '../components/cell';
-import {PrimaryButton, SecondaryButton} from '../components/button';
-import {SmallModal} from '../components/modal';
-import {BTPostData} from '../bt_communication/bt_data_sender';
-import {BTGetDataContinuous} from '../bt_communication/bt_data_receiver';
+import { PrimaryButton, SecondaryButton, PrimaryButtonSmall, TertiaryButton, ActionButton, RedActionButton, RegulationButton } from './components/button';
+import { SmallModal } from './components/modal';
+import { BTPostData } from './bt_communication/bt_data_sender';
+import {
+  BTGetDataContinuous,
+} from './bt_communication/bt_data_receiver';
+import { GoToPage } from './utils/nav';
 
 export const HomePage = props => {
   const [inputValue, setInputValue] = React.useState('');
@@ -47,70 +50,127 @@ export const HomePage = props => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Header>
+      {/* <Header>
         <H1>Monitracer</H1>
         <Body>Versão para testes de desenvolvimento</Body>
-      </Header>
+      </Header> */}
       <ScrollView>
         <VSeparator half />
-
-        <VBox>
-          <CellTitleContainer>
-            <H3>Teste Simples</H3>
-          </CellTitleContainer>
-        </VBox>
 
         <CellContainer>
           <PrimaryButton
             onPress={handleSimpleTestPress}
-            title={'Enviar sinal simples'}
+            title={'PB enable'}
+            disable={false}
           />
         </CellContainer>
 
         <VSeparator half />
-
-        <VBox>
-          <CellTitleContainer>
-            <H3>Teste Complexo</H3>
-          </CellTitleContainer>
-        </VBox>
-
-        <CellContainer>
-          <TextInputCell
-            placeholder={'Insira aqui o valor a enviar via bluetooth'}
-            onChangeText={setInputValue}
-            value={inputValue}
-          />
-          <PrimaryButton
-            onPress={handleComplexTestPress}
-            title={'Enviar sinal complexo'}
-          />
-        </CellContainer>
-
-        <VSeparator half />
-
-        <VBox>
-          <CellTitleContainer>
-            <H3>Teste leitura bluetooth</H3>
-          </CellTitleContainer>
-        </VBox>
 
         <CellContainer>
           <PrimaryButton
             onPress={BTGetDataContinuous}
-            title={'INICIAR'}
+            title={'PB disable'}
+            disable={true}
+          />
+        </CellContainer>
+
+        <CellContainer>
+          <PrimaryButtonSmall
+            onPress={BTGetDataContinuous}
+            title={'PBS enable'}
+            disable={false}
+          />
+        </CellContainer>
+
+        <CellContainer>
+          <PrimaryButtonSmall
+            onPress={BTGetDataContinuous}
+            title={'PBS disable'}
             disable={true}
           />
         </CellContainer>
 
         <VSeparator half />
 
-        <SmallModal
-          message={'O sinal recebido foi: '}
-          handleClose={handleCloseModal}
-          visible={modalVisible}
-        />
+        <CellContainer>
+          <SecondaryButton
+            onPress={BTGetDataContinuous}
+            title={'SB enable'}
+            disable={false}
+          />
+        </CellContainer>
+
+        <CellContainer>
+          <SecondaryButton
+            onPress={BTGetDataContinuous}
+            title={'SB disable'}
+            disable={true}
+          />
+        </CellContainer>
+
         <VSeparator half />
+
+        <CellContainer>
+          <TertiaryButton
+            onPress={BTGetDataContinuous}
+            title={'TB enable'}
+            disable={false}
+          />
+        </CellContainer>
+
+        <CellContainer>
+          <TertiaryButton
+            onPress={BTGetDataContinuous}
+            title={'TB disable'}
+            disable={true}
+          />
+        </CellContainer>
+
+        <VSeparator half />
+
+        <CellContainer>
+          <ActionButton
+            onPress={BTGetDataContinuous}
+            title={'AB1 enable'}
+            disable={false}
+          />
+        </CellContainer>
+
+        <CellContainer>
+          <ActionButton
+            onPress={BTGetDataContinuous}
+            title={'AB1 disable'}
+            disable={true}
+          />
+        </CellContainer>
+
+        <VSeparator half />
+
+        <CellContainer>
+          <RedActionButton
+            onPress={BTGetDataContinuous}
+            title={'RAB enable'}
+            disable={false}
+          />
+        </CellContainer>
+
+        <CellContainer>
+          <RedActionButton
+            onPress={BTGetDataContinuous}
+            title={'RAB disable'}
+            disable={true}
+          />
+        </CellContainer>
+
+        <VSeparator half />
+
+        <CellContainer>
+          <RegulationButton
+            onPress={BTGetDataContinuous}
+          />
+        </CellContainer>
+
         <VBox>
           <CellTitleContainer>
             <H3>Testes</H3>
