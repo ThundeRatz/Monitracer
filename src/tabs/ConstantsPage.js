7c0aab80e8b516c42c888a838b196b279c6f65bb
DamaralHenrique <rique01101999@gmail.com>
2022-01-24 19:06:39 -0300

    🎨 ✨ Add 3 colums for constants list

--- src/tabs/ConstantsPage.js
+++ src/tabs/ConstantsPage.js
@@ -9,34 +9,33 @@
  * @date 09/2021
  */
 
-import {CellContainer} from '../components/cell';
 import React from 'react';
-import {SafeAreaView, ScrollView, Text, StyleSheet, View, TextInput, Dimensions, ColorPropType} from 'react-native';
+import {SafeAreaView, ScrollView, StyleSheet, View, TextInput, Dimensions} from 'react-native';
 import {GoToTab} from '../utils/nav';
-import { Body , H1 , H3} from '../components/typography';
+import { Body , H3} from '../components/typography';
 import { COLORS } from '../components/colors';
-import { PrimaryButton , SecondaryButton, TertiaryButton, ActionButton, RedActionButton, GreenActionButton} from '../components/button';
+import { PrimaryButton , SecondaryButton, ActionButton, RedActionButton, GreenActionButton} from '../components/button';
 import { ROTATION } from '../components/rotation.js';
-import { SPACING } from '../components/grid.js';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { PROPOTION } from '../components/trigonometry';
 
 export const ConstantsPage = props => {
 
   //Constantes que virão do servidor
-  let constantTypes = ["P angular", "P angular", "I angular", "I angular", "D angular", "D angular", 
-  "P angular", "P angular", "I angular", "I angular", "D angular", "D angular",
-  "P angular", "P angular", "I angular", "I angular", "D angular", "D angular"];
+  let constantTypes = ["Kp reta", "Ki reta", "Kd reta", "Kp curva", "Ki curva", "Kd curva",
+  "Kp Zi-Za", "Ki Zi-Za", "Kd Zi-Za", "Kp Velocity", "Ki Velocity", "Kd Velocity"];
 
-  //Arranja o array de labels em duplas (matriz 2 x n/2) para dividir em 2 colunas na tela
+  //Arranja o array de labels em trios (matriz 3 x n/3) para dividir em 3 colunas na tela
   const arrayToMatrix = () => { 
     let constantCouples = [];
 
     constantTypes.forEach( (element, index, array) => {
-      if (index%2 == 0){
+      if (index%3 == 0){
         if(index == array.length - 1) {
-          constantCouples.push([array[index],null]);
+          constantCouples.push([array[index],null,null]);
+        } else if(index == array.length - 2){
+          constantCouples.push([array[index],array[index+1],null]);
         } else {
-          constantCouples.push([array[index],array[index+1]]);
+          constantCouples.push([array[index],array[index+1],array[index+2]]);
         }
       } 
     })
@@ -56,12 +55,13 @@ export const ConstantsPage = props => {
           </View>
         </View>
       </View>
-      
+
       <View style={styles.constantsInputContainer}>
         <ScrollView>
           {constantCouples.map((element, index) => {
             return (
               <View style = {styles.tableRow} key={index}>
+
                 <View style = {styles.tableCell}>  
                   <View style = {styles.textView}>
                     <Body>{element[0]}</Body>
@@ -74,14 +74,24 @@ export const ConstantsPage = props => {
                 
                 <View style = {styles.tableCell}>  
                   <View style = {styles.textView}>
-                    <Body>{element[0]}</Body>
+                    <Body>{element[1]}</Body>
                   </View>
                   
                   <View style = {styles.textInputView}>
                     <TextInput style={styles.textInput}/>
                   </View>
-
                 </View>
+
+                <View style = {styles.tableCell}>  
+                  <View style = {styles.textView}>
+                    <Body>{element[2]}</Body>
+                  </View>
+                  
+                  <View style = {styles.textInputView}>
+                    <TextInput style={styles.textInput}/>
+                  </View>
+                </View>
+
               </View>
             );
           })}
@@ -102,10 +112,10 @@ export const ConstantsPage = props => {
         </View>
         <View style={styles.buttonRow} >
           <View style = {styles.buttonCell}>  
-            <PrimaryButton title="Run"/>
+            <PrimaryButton title="RUN"/>
           </View>
           <View style = {styles.buttonCell}>  
-            <SecondaryButton title="Stop"/>
+            <SecondaryButton title="STOP"/>
           </View>
         </View> 
       </View>
@@ -124,16 +134,15 @@ const styles = StyleSheet.create({
   },
   titleContainer: {
     backgroundColor: COLORS.thunderBlue,
-    width: Dimensions.get('window').width / 0.989, //width / cos(8.5°)
-    top: - Dimensions.get('window').width * 0.14945 / 2, // width * tg(8,5°) / 2
-    height: Dimensions.get('window').width * 0.148, //width / sen(8,5°)
-    left: - Dimensions.get('window').width * 0.148 * 0.14945 //width * sin(8.5°) * tan(8.5°)
+    width: PROPOTION.width(Dimensions.get('window').width),
+    top: PROPOTION.top(Dimensions.get('window').width),
+    height: PROPOTION.height(Dimensions.get('window').width),
+    left: PROPOTION.left(Dimensions.get('window').width)
   },
   buttonsContainer: {
     flex: 1,
     flexDirection:'column',
     backgroundColor: COLORS.gray5,
-    
   },
   tableRow: {
     flexDirection : "row",
@@ -143,9 +152,8 @@ const styles = StyleSheet.create({
     paddingVertical: 10
   },
   tableCell: {
-    flexDirection : "row",
+    flexDirection : "column",
     flex: 1,
-    padding:1,
     alignItems: 'center',
     borderLeftColor: COLORS.gray3,
     borderLeftWidth: 1,
