beff4696996463ae1ba5f6b7b93a02051ca33b0f
guedeslucas1 <guedeslucas@usp.br>
2022-05-20 14:41:10 -0300

    🎨 Run prettier

--- src/tabs/EvaluationPage.js
+++ src/tabs/EvaluationPage.js
@@ -9,71 +9,75 @@
  * @date 05/2022
  */
 
-import {SecondaryButton, TertiaryButton, ActionButton} from '../components/button';
+import {
+  SecondaryButton,
+  TertiaryButton,
+  ActionButton,
+} from '../components/button';
 import {CellContainer} from '../components/cell';
 import React from 'react';
 import {SafeAreaView, ScrollView, View, StyleSheet} from 'react-native';
 import {GoToTab} from '../utils/nav';
-import {COLORS} from '../components/colors'
-import { backgroundColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
+import {COLORS} from '../components/colors';
+import {backgroundColor} from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 
 const encoder_evaluation = () => {
-  console.log("testando encoder");
-}
+  console.log('testando encoder');
+};
 
 const line_sensor_evaluation = () => {
-  console.log("testando sensor de linha");
-}
+  console.log('testando sensor de linha');
+};
 
 const motors_evaluation = () => {
-  console.log("testando motores");
-}
+  console.log('testando motores');
+};
 
 const lateral_marks_evaluation = () => {
-  console.log("testando marcacoes laterais")
-}
+  console.log('testando marcacoes laterais');
+};
 
 export const EvaluationPage = props => {
   let evaluateRoutines = [
     {
       test_name: 'Teste de Encoder',
-      routine: encoder_evaluation
+      routine: encoder_evaluation,
     },
     {
       test_name: 'Teste de sensores de linha',
-      routine: line_sensor_evaluation
+      routine: line_sensor_evaluation,
     },
     {
       test_name: 'Teste de motores',
-      routine: line_sensor_evaluation
+      routine: line_sensor_evaluation,
     },
     {
       test_name: 'Teste de Marcações Laterias',
-      routine: line_sensor_evaluation
-    }
-  ]
+      routine: line_sensor_evaluation,
+    },
+  ];
   return (
-    <ScrollView style = {styles.container}>
+    <ScrollView style={styles.container}>
       {evaluateRoutines.map((element, index) => {
-          return (
-            
-            <View style ={styles.buttonContainer} key={index}>
-              <ActionButton title= {element.test_name} onPress = {() => element.routine()}/>
-            </View>
-          )
-        })
-      }
+        return (
+          <View style={styles.buttonContainer} key={index}>
+            <ActionButton
+              title={element.test_name}
+              onPress={() => element.routine()}
+            />
+          </View>
+        );
+      })}
     </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop:50
-  }, 
+    paddingTop: 50,
+  },
   buttonContainer: {
     paddingHorizontal: 30,
     paddingVertical: 20,
-  }
-
-})
+  },
+});
