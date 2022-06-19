8417a57578c8d100d429bb6d3b81fe6bfed6ffbf
VanderSant <vanderson.santos@usp.br>
2022-06-19 14:57:39 -0300

    🤡  add "BT >" on console log

--- src/bt_communication/bt_data_sender.js
+++ src/bt_communication/bt_data_sender.js
@@ -29,7 +29,7 @@ const byte_final_signal_value = "fe"
  */
 export const BTPostHex = async msg => {
   let msg_header_tail = byte_initial_signal_value+msg+byte_final_signal_value
-  console.log("msg sent: " + msg_header_tail)
+  console.log("BT > msg sent: " + msg_header_tail)
   let buf = Buffer.from(msg_header_tail, 'hex');
   await BTPostData(buf);
 }
