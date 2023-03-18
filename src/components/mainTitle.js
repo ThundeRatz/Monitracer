bf71c3ba57c2b85a121fada4f448d36e78702d0d
DamaralHenrique <rique01101999@gmail.com>
2023-03-18 16:49:01 -0300

    ✨ Add MainTitle subtitle

--- src/components/mainTitle.js
+++ src/components/mainTitle.js
@@ -7,7 +7,7 @@ import {
 import {COLORS} from './colors';
 import {ROTATION} from './rotation.js';
 import {PROPOTION} from './trigonometry';
-import {H3} from './typography';
+import {H3, Body} from './typography';
 
 export const MainTitle = (props) => {
   return(
@@ -15,6 +15,9 @@ export const MainTitle = (props) => {
       <View style={ROTATION.OitoEMeia}>
         <View style={styles.titleContainer} paddingLeft={20}>
           <H3 color="white">{props.title}</H3>
+        </View>
+        <View style={styles.timerContainer}>
+          <Body color="black" bold>Tempo: {props.subtitle}ms</Body>
         </View>
       </View>
     </View>
@@ -29,4 +32,10 @@ const styles = StyleSheet.create({
       height: PROPOTION.height(Dimensions.get('window').width),
       left: PROPOTION.left(Dimensions.get('window').width),
   },
+  timerContainer:{
+     position: "absolute",
+     alignSelf: "flex-end",
+     right: 20,
+     top: 30,
+  }
 });
