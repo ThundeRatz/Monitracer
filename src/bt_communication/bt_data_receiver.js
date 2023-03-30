868666c5871b10a631dad6e18eba1918b593dfe0
DamaralHenrique <rique01101999@gmail.com>
2023-03-29 22:01:18 -0300

    🔨 Fix bluetooth protocol value area

--- src/bt_communication/bt_data_receiver.js
+++ src/bt_communication/bt_data_receiver.js
@@ -19,7 +19,7 @@ import { Buffer } from 'buffer';
 
 const HEADER_TAG = String.fromCharCode(parseInt("FF", 16));
 const TAIL_TAG = String.fromCharCode(parseInt("FE", 16));
-const LAP_TIME_TAG = String.fromCharCode(parseInt("BE", 16));
+const LAP_TIME_TAG = String.fromCharCode(parseInt("BE11", 16));
 
 /*****************************************
  * Public Functions
@@ -50,10 +50,16 @@ export const BTGetDataContinuous = () => {
     BluetoothSerial.readEvery(
       (data, intervalId) => {
         if(!!data){
+          console.log('A mensagem recebida foi: ' + data);
+          var hex = 0;
+          for(var i=0;i<data.length;i++) {
+              hex += data.charCodeAt(i).toString(16);
+          }
+          console.log('A mensagem recebida em hex foi: ' + hex);
           const header = data.slice(0, 1)
           const tail = data.slice(-1)
           if(header == HEADER_TAG && tail == TAIL_TAG){
-            const msg = data.slice(2,-1)
+            const msg = data.slice(4,-1)
             
             var value = 0;
             for(var i=0;i<msg.length;i++) {
@@ -62,7 +68,7 @@ export const BTGetDataContinuous = () => {
                 value += (parseInt(char_code[0], 16)-1) * (15 ** (current_position + 1)) + (parseInt(char_code[1], 16)-1) * ( 15 ** current_position)
             }
             console.log('A mensagem recebida em hex foi: ' + value);
-            const type = data.slice(1,2)
+            const type = data.slice(1,4)
             if(type == LAP_TIME_TAG){
               setLapTime(value)
             }
